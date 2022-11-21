d529c479c383dfc040f98d8a8c22d2f3e19cfcad
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-21 11:13:06 +0100

    Scaffolding

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <AppHeader/>
+  <main>
+
+  </main>
+  <AppFooterVue/>
+</template>
+
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
+
+
+  export default {
+    components: { 
+      AppHeader,
+      AppFooter 
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Header
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
