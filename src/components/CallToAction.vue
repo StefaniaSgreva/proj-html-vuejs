239b933b58ca94aff8cd35cf8e3f0e042a0df051
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-21 17:22:04 +0100

    added 'call to action' section

--- src/components/CallToAction.vue
+++ src/components/CallToAction.vue
@@ -1,6 +1,8 @@
 <template>
-    <section>
-        Call to Action
+    <section id="call-to-action">
+        <h2>Want to work with us? Send us <span class="font-italic">a message!</span></h2>
+        <p>Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor incididunt ut labore et dolore. Ut enim ad minim veniam, quis nostrud exercitatian ullamco laboris nisi ut aliquip ex ea commo.</p>
+        <button class="btn">Contact</button>
     </section>
 </template>
 
@@ -11,5 +13,35 @@
 </script>
 
 <style lang="scss" scoped>
+@use '../assets/styles/partials/variables' as *;
+@use '../assets/styles/partials/mixins' as *;
+
+#call-to-action{
+    width: 100%;
+    height: 450px;
+    background-image: url(../assets/images/h-2-background-1.jpg);
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    text-align: center;
+    @include dflex-center-col;
+
+    h2{
+        font-size: 3rem;
+        font-weight: lighter;
+    }
+    p{
+        width: 1200px;
+        line-height: 2rem;
+        color: $text-color-gray;
+        padding: 1.5rem 0;
+    }
+    button{
+        padding: .85rem 2.8rem;
+        background-color: rgba($color: #c0e1cf, $alpha: 0.6);
+        
+        
+    }
+}
 
 </style>
